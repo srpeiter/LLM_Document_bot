11d0a43ded642b36055d18a1fb22bcfab02592c2
Sarwan Peiter <sarwanpeiter@Sarwans-MacBook-Pro.local>
2023-10-10 01:43:47 +0200

    modified system prompt for llama

--- LLM_UI/chain_factory.py
+++ LLM_UI/chain_factory.py
@@ -4,6 +4,11 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from langchain.llms import HuggingFaceHub, LlamaCpp, OpenAI
 from langchain.memory import ConversationBufferMemory
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate,
+)
 from langchain.prompts.prompt import PromptTemplate
 
 from LLM_UI.custom_types import LLMName
@@ -16,6 +21,18 @@ llm_map: Dict[str, Any] = {
     "chat_open_ai": ChatOpenAI,
 }
 
+_TEMPLATE_PROMPT = """ \
+<s>[INST] <<SYS>>
+Je bent een behulpzame, respectvolle en eerlijke assistant. Antwoord altijd zo behulpzaam mogelijk.
+Je antwoorden mogen geen schadelijke, onethische, racistische, seksistische, gevaarlijke of illegale inhoud bevatten. Zorg ervoor dat je antwoorden sociaal onbevooroordeeld en positief.
+
+Gebruik de volgende stukjes context om de vraag van de gebruiker te beantwoorden. De antwoorden moeten in het Nederlands zijn.
+Also een vraag nergens op slaat of feitelijk niet coherent is, leg dan uit waarom in plaats van iets niet correct te antwoorden. Also je het antwoord op een vraag niet weet, deel dan geen onjuiste informative.
+<</SYS>>
+context : {context}
+question: {question} [/INST]
+"""
+
 
 def chain_factory(llm_name: LLMName, vector_db: VectorDB, **kwargs):
     llm = llm_map[llm_name](**kwargs)
@@ -23,6 +40,7 @@ def chain_factory(llm_name: LLMName, vector_db: VectorDB, **kwargs):
         memory_key="chat_history", output_key="answer", return_messages=True
     )
 
+    chat_prompt = PromptTemplate.from_template(_TEMPLATE_PROMPT)
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
         retriever=vector_db.get_db.as_retriever(
@@ -32,5 +50,6 @@ def chain_factory(llm_name: LLMName, vector_db: VectorDB, **kwargs):
         return_generated_question=True,
         return_source_documents=True,
         verbose=True,
+        combine_docs_chain_kwargs={"prompt": chat_prompt},
     )
     return conversation_chain
